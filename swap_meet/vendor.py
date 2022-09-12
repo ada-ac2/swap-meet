f493e32d0c50c78083d60f96dfaceb26445ce2de
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2022-09-12 14:41:24 -0700

    Wave 1 completed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,17 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        if inventory == None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item not in self.inventory:
+            return None
+            
+        self.inventory.remove(item)
+        return item
