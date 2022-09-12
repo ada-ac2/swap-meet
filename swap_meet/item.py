9ed2a07fac3e021149a8301060005865a1f592f2
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2022-09-12 14:45:20 -0700

    Wave 2 complete

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,8 @@
+import uuid
+
 class Item:
-    pass
+    def __init__(self, id=None) -> None:            
+        self.id = id if id else uuid.uuid4().int
+
+    def get_category(self):
+        return type(self).__name__
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -12,6 +12,13 @@ class Vendor:
     def remove(self, item):
         if item not in self.inventory:
             return None
-            
+
         self.inventory.remove(item)
         return item
+
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if item.id == id:
+                return item
+
+        return None
