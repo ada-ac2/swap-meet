280d6cbeb1e87b50ee803ce374473183a51aa800
Becca Elenzil <becca@adadevelopersacademy.org>
2022-09-13 10:59:33 -0700

    complete wave 2

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,13 @@
+import uuid
+
 class Item:
-    pass
+    
+
+    def __init__(self, id=None):
+        if id == None:
+            self.id = uuid.uuid1().int
+        else:
+            self.id=id
+
+    def get_category(self):
+        return type(self).__name__
