96e084981ea48b106adaaeb3cc03a65eeb25b685
dalia <dnabil89@gmail.com>
2022-12-01 14:34:18 -0800

    completed wave 1: added remove(item) method and passed all wave 1 tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,12 +1,20 @@
 class Vendor:
     def __init__(self, inventory = None):
+        #validate that iventory is a list
         self.inventory = [] if inventory == None else inventory 
 
     def add(self, item):
         self.inventory.append(item)
         return item
 
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+        else:
+            return None
+        
+        return item
 
 # friend = Vendor(["pj", "pc"])
-# friend.add("bracelet")
+# friend.remove("bracelet")
 # print(friend.inventory)
