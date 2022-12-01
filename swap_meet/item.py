6179dacbebb1c8c10dd663c0969064fe938c0ac9
Elaine <sosohn@gmail.com>
2022-12-01 15:16:29 -0800

    Add Vender and Item class and Finish tests wave01 and wave02

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,15 @@
+import uuid
+
 class Item:
-    pass
+    # TODO understand how to apply id to inventory
+    
+    def __init__(self, id=None):
+        if not id:
+            id = uuid.uuid4().int
+        self.id = id
+    
+    def get_category(self):
+        return f'{self.__class__.__name__}'
+    
+
+
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,21 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+    
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item not in self.inventory:
+            return None
+        else:
+            self.inventory.remove(item)
+            return item
+
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if item.id == id:            
+                return item
+        return None
+
