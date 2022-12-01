6bc4828e1994bba90a7009c6808fa5dbf6a57670
Elaine <sosohn@gmail.com>
2022-12-01 15:18:19 -0800

    delete comment

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,8 +1,6 @@
 import uuid
 
-class Item:
-    # TODO understand how to apply id to inventory
-    
+class Item:    
     def __init__(self, id=None):
         if not id:
             id = uuid.uuid4().int
