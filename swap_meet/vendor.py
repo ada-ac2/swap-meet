11138dc4c27790ac12cbef6ec0b163b11db493ac
nadxelle <nadxellehernandez@gmail.com>
2022-12-01 14:38:30 -0800

    First implementation of Vendor class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,20 @@
 class Vendor:
     def __init__(self, inventory = None) :
         self.inventory = inventory if inventory is not None else []
+
+    def add(self, item):
+        if not item:
+            return item
+
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):  
+        if not item:
+            return item
+
+        if item not in self.inventory:
+            return None
+
+        self.inventory.remove(item)
+        return item
