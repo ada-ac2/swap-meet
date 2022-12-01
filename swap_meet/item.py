fe20bd5ff821043182647c27e1349d6ad9636a73
larissa <larza.ault@gmail.com>
2022-12-01 14:20:44 -0800

    Added code for waves 1 and 2

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,10 @@
+import uuid
+
 class Item:
-    pass
+    def __init__(self, id=None):
+        self.id = id if id else uuid.uuid4().int
+    
+    def get_category(self):
+        return f"{self.class_name}" # string holding name of the class?
+    
+    #### wave 3
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,20 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        self.inventory = inventory if inventory else []
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if not self.inventory[item]:
+            return None
+        else: 
+            self.inventory.remove(item)
+            return item
+    
+    def get_by_id(self, id):
+        if not self.inventory[id]:
+            return None
+        else:
+            return self.inventory[id]
