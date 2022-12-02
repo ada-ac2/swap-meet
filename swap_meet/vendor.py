a2f37ff821167b227f3d4be706038d2f0c49ed55
Ya-Juan Ruan <yajuan@decltype.org>
2022-12-01 20:08:24 -0800

    Finish wave 1 and wave 1 test file

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -4,4 +4,21 @@ class Vendor:
         # Inventory attribute should be optional. 
         # If there is no inventory argument was passed into. The default value of inventory will be an empty list.
         self.inventory = inventory if inventory else []
+    
+    def add(self, new_item):
+        if not new_item:
+            raise ValueError("Item can't be empty.")    
+        self.inventory.append(new_item)
+        return new_item
+
+    def remove(self, item):
+        # when there are two or more duplicate items in the inventory, we'll only remove one.
+        try:
+            self.inventory.remove(item)
+            return item       
+        except ValueError:  # When the item is not in inventory will raise value error.
+            return None
+
+
+
 
