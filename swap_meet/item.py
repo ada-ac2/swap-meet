ff78fb7f8115dd58b4fb0e7a267af4995aff0746
Ya-Juan Ruan <yajuan@decltype.org>
2022-12-01 22:22:10 -0800

    Finish item class for wave 2

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,12 @@
+import uuid
+
 class Item:
-    pass
+    def __init__(self, id=None):
+        self.id = id if id else uuid.uuid4().int
+
+    
+    def __str__(self):
+        return self.__class__.__name__
+    
+    def get_category(self):
+        return self.__str__()
