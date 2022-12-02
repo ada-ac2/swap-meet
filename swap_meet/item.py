b43ef4dfba26970bc036b7fd7435a36121744e41
Yufei Bao <ybao1005@gmail.com>
2022-12-02 12:40:45 -0800

    Wave5 Completed; Wave5 Test Completed

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,11 +1,24 @@
 import uuid 
 
 class Item:
-    def __init__(self, id = None):
+    def __init__(self, id = None, condition = 0):
         self.id = id if id else uuid.uuid1().int
+        self.condition = condition 
 
     def get_category(self): 
         return "Item"
+
+    def condition_description(self):
+        if self.condition == 0:
+            return "New"
+        if self.condition == 1:
+            return "Close to new"
+        if self.condition == 2 or self.condition == 3:
+            return "Normal Wear"
+        if self.condition == 4:
+            return "Used"
+        else:
+            return "Heavily Used"
 
     # Override str from python built-in  
     def __str__(self): 
