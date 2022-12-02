0e44a3829727b3368d46835f6ab5f1f6cd7fa2d4
Ya-Juan Ruan <yajuan@decltype.org>
2022-12-02 12:34:50 -0800

    Update __str__ method

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -5,9 +5,8 @@ class Item:
         self.id = id if id else uuid.uuid4().int
         
 
-    
     def __str__(self):
-        return f"An object of type {self.__class__.__name__} with id {self.id}"
+        return f"An object of type {self.get_category()} with id {self.id}"
     
     def get_category(self):
         return self.__class__.__name__
