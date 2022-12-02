c5ea8f53d26551888dd04c3471e0a35f30ddf8a1
Cindy Truong <cindythitruong@gmail.com>
2022-12-02 10:30:41 -0800

    wave 02

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,8 @@
+import uuid
+
 class Item:
-    pass
+    def __init__(self, id=None):
+        self.id = id if id is not None else int(uuid.uuid4())
+
+    def get_category(self):
+        return "Item"
