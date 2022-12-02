f6790750d929e74ebbaedc136f17e83c85f7d8c9
dalia <dnabil89@gmail.com>
2022-12-02 10:18:49 -0800

    implemented wave 3 (swap_items method in vendor class) and passed all tests

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -2,7 +2,13 @@ import uuid
 
 class Item:
     def __init__(self, id = None):
+        #maybe check if id already exists so that there won't be duplicates??
         self. id = uuid.uuid4().int if id is None else id
 
     def get_category(self):
         return self.__class__.__name__
+
+    def __repr__(self):
+        return f"An object of type {self.get_category()} with id {self.id}"
+
+    
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -24,3 +24,13 @@ class Vendor:
         #         return item
 
         # return None
+
+    def swap_items(self, other_vendor, self_item, other_vendor_item):
+        if self_item in self.inventory and other_vendor_item in other_vendor.inventory:
+            self.remove(self_item)
+            self.add(other_vendor_item)
+            other_vendor.remove(other_vendor_item)
+            other_vendor.add(self_item)
+            return True
+        else:
+            return False
