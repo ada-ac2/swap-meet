e798b5fab2b78e12f1630cc9389595620f380420
mckay <mckaybwarren@gmail.com>
2022-12-02 16:16:35 -0500

    wave 6 complete

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -42,3 +42,38 @@ class Vendor:
             other_vendor.inventory.append(my_first_item)
             self.inventory.append(their_first_item)
             return True
+
+    def get_by_category(self, category):
+        items_in_category = []
+        for item in self.inventory:
+            if item.get_category() == category:
+                items_in_category.append(item)
+        return items_in_category
+
+    def get_best_by_category(self, category):
+        sorted_category_items = self.get_by_category(category)
+        if not sorted_category_items:
+            return None
+        else:
+            #is there a way to access the item's condition without a lambda fn here?
+            return max(sorted_category_items, key=lambda item: item.condition)
+
+    def swap_best_by_category(self, other_vendor, my_priority, their_priority):
+        item_for_me = other_vendor.get_best_by_category(my_priority)
+        item_for_them = self.get_best_by_category(their_priority)
+
+        return self.swap_items(other_vendor, item_for_them, item_for_me)
+
+    def display_inventory(self, category=""):
+        inventory_summaries = ""
+        if not category:
+            for i in range(len(self.inventory)):
+                inventory_summaries += f"{i+1}. {self.inventory[i].__str__()}\n"
+        else:
+            for i in range(len(self.inventory)):
+                if self.inventory[i].get_category() == category:
+                    inventory_summaries += f"{i+1}. {self.inventory[i].__str__()}\n"
+        if inventory_summaries == "":
+            print("No inventory to display.")
+        else:
+            print(inventory_summaries)
