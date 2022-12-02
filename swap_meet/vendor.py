02b1fa9d551080525ef33fa746b64e10df89008c
Jennifer Dai <jendai173@gmail.com>
2022-12-02 10:18:36 -0800

    vendor.py updated add and remove methods

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,19 @@
 class Vendor:
-    pass
+    def __init__(self,inventory = None):
+        if inventory is not None:
+            self.inventory = inventory
+        else:
+            self.inventory = []
+
+#add new item and return inventory list
+    def add(self,add_item):
+        self.inventory.append(add_item)
+        return add_item
+
+#remove duplicates from inventory list if any, else return None
+    def remove(self,check_item):
+        if check_item in self.inventory:
+            self.inventory.remove(check_item)
+            return check_item
+        else:
+            return None
