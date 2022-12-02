33fe8f8d3dded15b913d278073838a6f0831a5f8
mckay <mckaybwarren@gmail.com>
2022-12-01 20:38:51 -0500

    waves 1, 2, and 3 complete

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -13,3 +13,21 @@ class Vendor:
             return item
         else:
             return None
+
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if item.id == id:
+                return item
+        return None
+
+    def swap_items(self, other_vendor, my_item, their_item):
+        if (my_item not in self.inventory or their_item not in other_vendor.inventory):
+            return False
+        else:
+            #using pop to return removed items and store in swapped_item vars
+            my_swapped_item = self.inventory.pop(self.inventory.index(my_item))
+            their_swapped_item = other_vendor.inventory.pop(other_vendor.inventory.index(their_item))
+            
+            other_vendor.inventory.append(my_swapped_item)
+            self.inventory.append(their_swapped_item)
+            return True
