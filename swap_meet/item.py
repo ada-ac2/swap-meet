4df6934e3dd130432b7ac4b8ed700737c2220816
Soumya Sah <sah.soumya@gmail.com>
2022-12-02 11:23:56 -0800

    Adding 1st implementation of vendor.py and item.py. Tests in test_wave_01 and test_wave_02 pass

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,8 @@
+import uuid
 class Item:
-    pass
+    def __init__(self, id=None):
+        unique_identifier = uuid.uuid4()
+        self.id = id if id else unique_identifier.int
+    def get_category(self):
+        return self.__class__.__name__
+    
