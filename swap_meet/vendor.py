9eda01956f0508fb10144bca604ec44d75b3de35
Yufei Bao <ybao1005@gmail.com>
2022-12-02 09:41:45 -0800

    Wave1 Completed; Wave1 Test Completed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,23 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        self.inventory = inventory if inventory else [] 
+    
+    def add(self, item): 
+        # Check input validation 
+        item = item.strip() 
+        if not item: 
+            raise ValueError("Invalid Input")
+        # Check duplicated item 
+        if item in self.inventory: 
+            raise ValueError("Item already exist in the current inventory, item has not been added")
+
+        self.inventory.append(item) 
+
+        return item 
+
+    def remove(self, item): 
+        if item not in self.inventory: 
+            return None 
+            
+        self.inventory.remove(item)
+        return item 
