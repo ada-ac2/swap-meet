14f118759c7e711ef73d4e787022e57a5c976d0d
Ya-Juan Ruan <yajuan@decltype.org>
2022-12-01 19:46:36 -0800

    Create vendor class consructor

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,7 @@
 class Vendor:
-    pass
+    
+    def __init__(self, inventory=None):
+        # Inventory attribute should be optional. 
+        # If there is no inventory argument was passed into. The default value of inventory will be an empty list.
+        self.inventory = inventory if inventory else []
+
