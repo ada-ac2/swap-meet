3b61f9f74dd649a381653fab6f13a01341ace154
Ya-Juan Ruan <yajuan@decltype.org>
2022-12-02 12:17:59 -0800

    Revise __str__ method for wave 5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -3,10 +3,11 @@ import uuid
 class Item:
     def __init__(self, id=None):
         self.id = id if id else uuid.uuid4().int
+        
 
     
     def __str__(self):
-        return f"An object of type Item with id {self.id}"
+        return f"An object of type {self.__class__.__name__} with id {self.id}"
     
     def get_category(self):
         return self.__class__.__name__
