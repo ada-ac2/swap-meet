2339cc9b755afd05eed32f2b37e57023db0d296f
nadxelle <nadxellehernandez@gmail.com>
2022-12-02 21:55:58 -0800

    finished wave5 classes and tests

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,11 +1,19 @@
 import uuid
 
 class Item:
-    def __init__(self, id= None) :
+    cond_descriptions = ["Poor",
+                         "Heavily Used",
+                         "Fair",
+                         "Good"
+                         "Very Good",
+                         "Mint"]
+
+    def __init__(self, id= None, condition=0) :
         if id is not None:
             self.id = id
         else:
             self.id = uuid.uuid4().int
+        self.condition = condition
 
     def get_category(self):
         return self.__class__.__name__
@@ -13,3 +21,6 @@ class Item:
     def __str__(self) -> str:
         str_item = f"An object of type {self.get_category()} with id {self.id}"
         return str_item
+
+    def condition_description(self):
+        return self.cond_descriptions[int(self.condition-1)]
