7c2e3d3cfa00775e031087dc7ba192017850ea3e
Yufei Bao <ybao1005@gmail.com>
2022-12-03 00:16:14 -0800

    Wave6 Completed; Wave6 Test Completed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -28,7 +28,7 @@ class Vendor:
         return None # Return None if no matching item 
 
     def swap_items(self, other_vendor, my_item, their_item): 
-        if my_item not in self.inventory or their_item not in other_vendor.inventory: 
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
             return False 
 
         # Removes my_item from my inventory, add to other_vendor's inventory 
@@ -44,15 +44,46 @@ class Vendor:
         # Check if either vendor's inventory is empty 
         if len(other_vendor.inventory) == 0 or len(self.inventory) == 0:
             return False 
-            
-        # Removes the first item from both vendors' inventory 
-        other_first_item = other_vendor.remove(other_vendor.inventory[0])
-        my_first_item = self.remove(self.inventory[0])
-        # Add/Swap items 
-        self.add(other_first_item)
-        other_vendor.add(my_first_item)
 
-        return True 
+        return self.swap_items(other_vendor, self.inventory[0], other_vendor.inventory[0])
+
+    def get_by_category(self, category): 
+        result = [] 
+
+        for item in self.inventory: 
+            item_category = item.get_category()
+            if item_category == category: 
+                result.append(item) #Adds the object that has the requested category name 
+
+        return result 
+    
+    def get_best_by_category(self, category): 
+        objects_in_category = self.get_by_category(category)
+        if not objects_in_category: 
+            return None 
+        
+        return max(objects_in_category, key=lambda object: object.condition)
+
+        # My own logic to find the highest condition: 
+        # best_condition = objects_in_category[0].condition 
+        # best_condition_object = objects_in_category[0] 
+
+        # for object in objects_in_category: 
+        #     condition = object.condition
+        #     if condition == 5: # Best condition is 5, no need to check for the rest 
+        #         return object  
+        #     if condition > best_condition: 
+        #         best_condition = condition 
+        #         best_condition_object = object 
+
+        # return best_condition_object 
+
+    def swap_best_by_category(self, other_vendor, my_priority, their_priority):
+        my_best_item = self.get_best_by_category(their_priority)
+        their_best_item = other_vendor.get_best_by_category(my_priority)
+
+        return self.swap_items(other_vendor, my_best_item, their_best_item)
+
 
 
 
