34f25f22c1cf1436410254963e30df9944ecb016
Xuan Hien Pham <phamxuanhien02@gmail.com>
2022-12-03 16:14:03 -0500

    passed test_wave_05

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,8 +1,8 @@
 from swap_meet.item import Item
 
 class Clothing(Item):
-    def __init__(self, id=None, fabric=None):
-        super().__init__(id)
+    def __init__(self, id=None, condition=None, fabric=None):
+        super().__init__(id, condition)
         self.fabric = "Unknown" if fabric is None else fabric
 
     def get_category(self):
@@ -10,3 +10,6 @@ class Clothing(Item):
 
     def __str__(self):
         return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
+
+    def condition_description(self):
+        return super().condition_description()
--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,4 +1,16 @@
 from swap_meet.item import Item
 
 class Decor(Item):
-    pass
+    def __init__(self, id=None, condition=None, width=None, length=None):
+        super().__init__(id, condition)
+        self.width = 0 if width is None else width
+        self.length = 0 if length is None else length
+
+    def get_category(self):
+        return super().get_category()
+
+    def __str__(self):
+        return f"An object of type Decor with id {self.id}. It takes up a {self.width} by {self.length} sized space."
+
+    def condition_description(self):
+        return super().condition_description()
