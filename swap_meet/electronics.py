6c9b5d3a227f15637885f8cf29c375f97b0e6cdd
Ya-Juan Ruan <yajuan@decltype.org>
2022-12-03 15:06:32 -0800

    Finish electronics class for wave 5

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,8 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+class Electronics(Item):
+    def __init__(self, id=None, type="Unknown"):
+        super().__init__(id)
+        self.type = type
+    
+    def __str__(self):
+        return (f"{super().__str__()}. This is a {self.type} device.")
