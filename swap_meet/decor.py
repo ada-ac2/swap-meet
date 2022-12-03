8c8389f77724e5d8bba75fd2cfa06c2d660c8700
nadxelle <nadxellehernandez@gmail.com>
2022-12-02 21:32:15 -0800

    Decor class and tests done

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,11 @@
-class Decor:
-    pass
+from swap_meet.item import Item
+class Decor(Item):
+    def __init__(self, id=None, width=0, length=0):
+        super().__init__(id)
+        self.width = width
+        self.length = length
+
+    def __str__(self) -> str:
+        decor_str = super().__str__()
+        decor_str += f". It takes up a {self.width} by {self.length} sized space."
+        return decor_str
