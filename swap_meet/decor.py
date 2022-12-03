e73f74b9551fd33fe9d1a7013f53b5b2d1941fbc
Ya-Juan Ruan <yajuan@decltype.org>
2022-12-03 14:58:00 -0800

    Finish Decor class for wave 5

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,10 @@
-class Decor:
-    pass
+from swap_meet.item import Item
+
+class Decor(Item):
+    def __init__(self, id=None, width=0, length=0):
+        super().__init__(id)
+        self.width = width
+        self.length = length
+
+    def __str__(self):
+        return (f"{super().__str__()}. It takes up a {self.width} by {self.length} sized space.")
