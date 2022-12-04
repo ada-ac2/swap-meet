4a01626219f91033818516a86788b75ce32a39d3
Cindy Truong <cindythitruong@gmail.com>
2022-12-03 18:34:55 -0800

    test wave 3 and tests

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -4,5 +4,8 @@ class Item:
     def __init__(self, id=None):
         self.id = id if id is not None else int(uuid.uuid4())
 
+    def __str__(self):
+        return f'An object of type Item with id {self.id}'
+
     def get_category(self):
         return "Item"
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -19,3 +19,18 @@ class Vendor:
             if item.id == id:
                 return item
         return None
+    #            fatimah.swap_items(jolie, item_b, item_d)
+    def swap_items(self, other_vendor, my_item, thier_item):
+        #input: other_vendor: instance of another Vendor
+        #       my_item: instance of an Item, what item 'this' vendor wants to swap
+        #       thier_item: instance of an Item, what item other_vendor wants to swap
+        #output: return True or False
+        if (my_item not in self.inventory) or (thier_item not in other_vendor.inventory):
+            return False
+        else:
+            self.inventory.remove(my_item)
+            other_vendor.inventory.append(my_item)
+            self.inventory.append(thier_item)
+            other_vendor.remove(thier_item)
+            return True
+
