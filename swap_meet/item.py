f86006245241645958d576a7281a350c4476ae8b
Ya-Juan Ruan <yajuan@decltype.org>
2022-12-03 16:45:45 -0800

    Add condition attributes and condition_description method for wave 5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,8 +1,10 @@
 import uuid
+import math
 
 class Item:
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition=0):
         self.id = id if id else uuid.uuid4().int
+        self.condition = condition
         
 
     def __str__(self):
@@ -10,3 +12,18 @@ class Item:
     
     def get_category(self):
         return self.__class__.__name__
+
+    def condition_description(self):
+        match math.floor(self.condition):
+            case 0:
+                return ("condition is 0")
+            case 1:
+                return ("condition is 1")
+            case 2:
+                return ("condition is 2")
+            case 3:
+                return ("condition is 3")
+            case 4:
+                return ("condition is 4")
+            case other:
+                return ("Lucky one with best condition")
