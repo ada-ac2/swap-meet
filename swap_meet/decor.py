fac82c1e55119d79e129d274c41e1f2bef2d5cec
Catherine Bandarchuk <kate@sealucky.com>
2022-12-05 15:38:19 -0800

    Create classes Electronics, Decor, Clothing using inheretence of Item Class. Test_wave5 passed.

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,16 @@
-class Decor:
-    pass
+from .item import Item
+
+class Decor(Item):
+    def __init__(self, id = None, condition = None, width = None, length = None):
+        super().__init__(id, condition)
+        if not width:
+            self.width = 0
+        else:
+            self.width = width
+        if not length:
+            self.lenght = 0
+        else:
+            self.lenght = length
+
+    def __str__(self):
+        return f"An object of type {super().get_category()} with id {str(self.id)}. It takes up a {str(self.width)} by {str(self.lenght)} sized space."  
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,15 +1,31 @@
 import uuid
 
 class Item:
-    def __init__( self, id = None, ):
+    def __init__( self, id = None, condition = None):
         if id == None:
             self.id = uuid.uuid1().int
         else:
             self.id = id
+        
+        if condition == None:
+            self.condition = 0
+        elif condition > 5 or condition < 0:
+            raise IndexError("Please choose condition in range 0 - 5.")
+        else:
+            self.condition = condition
 
-    def __str__(self):
-        return f"An object of type Item with id {str(self.id)}"      
 
     def get_category( self ):
-        category = self.__class__
-        return str(category.__name__)
+        return self.__class__.__name__
+
+    def __str__(self):
+        return f"An object of type {self.get_category()} with id {str(self.id)}"
+    
+    def condition_description(self):
+        condition_list = ["New",
+                        "Used - Great Condition",
+                        "Used - Very Good Condition",
+                        "Used - Good Condition",
+                        "Heavily Used", 
+                        "Poor Condition"]
+        return condition_list[self.condition]
