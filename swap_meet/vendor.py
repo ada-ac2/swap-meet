bbc5ee4f95343d09fdc317979164d38a45d0bf40
Catherine Bandarchuk <kate@sealucky.com>
2022-12-04 21:38:24 -0800

    Create method swap_first_item in vendor class. Test_wave4 pass.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -12,19 +12,21 @@ class Vendor:
     def remove( self, item):
         if self.inventory == None or item not in self.inventory:
             return None
+        
         self.inventory.remove(item)
         return item
     
     def get_by_id(self, item_id):
         if not self.inventory:
             return None
+
         for item in self.inventory:
             if item.id == item_id:
                 return item
+        
         return None
 
-    def swap_items(self, other_vendor, my_item, their_item):
-        
+    def swap_items(self, other_vendor, my_item, their_item):        
         if not self.inventory or \
             not other_vendor.inventory or \
             not my_item or not their_item or \
@@ -36,5 +38,17 @@ class Vendor:
         other_vendor.add(my_item)
         other_vendor.remove(their_item)
         self.add(their_item)
+        return True
+
+    def swap_first_item( self, other_vendor):
+        if not self.inventory or \
+        not other_vendor.inventory:
+            return False
+        my_item = self.inventory[0]
+        their_item = other_vendor.inventory[0]
+        self.remove(self.inventory[0])
+        self.add(their_item)
+        other_vendor.remove(their_item)
+        other_vendor.add(my_item)
         return True
 
