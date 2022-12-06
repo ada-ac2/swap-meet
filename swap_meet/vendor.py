bac22de1ddf38e7b186e2a31d0acd8f6e090e4c8
Soumya Sah <sah.soumya@gmail.com>
2022-12-05 22:54:42 -0800

    Add swap_items method to vendor.py Improve method get_by_id in vendor.py

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,16 +1,29 @@
 class Vendor:
     def __init__(self, inventory=None):
         self.inventory = inventory if inventory else []
+
     def add(self, item):
-        self.inventory.append(item)
+        if not item:
+            return None
+        self.inventory.append(item) 
         return item
+
     def remove(self, item):
         if item not in self.inventory:
             return None
         self.inventory.remove(item)
         return item
+
     def get_by_id(self, id):
-        for item in self.inventory:
-            if item.id == id:
-                return item 
-        return None
+        if not id:
+            return None
+        return next((item for item in self.inventory if item.id == id), None)
+
+    def swap_items(self, other_vendor, my_item, their_item):
+        if not my_item or not their_item:
+            return False
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+        self.add(other_vendor.remove(their_item))
+        other_vendor.add(self.remove(my_item))
+        return True
