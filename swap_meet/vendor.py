d86e0c329c39f85a819b19def2b941fc1b77e51c
diana <arreoladiana11@gmail.com>
2022-12-05 22:16:29 -0800

    wave 03: Add swap_items instance method

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -7,14 +7,24 @@ class Vendor:
         return item
 
     def remove(self, item):
-        if item in self.inventory:
-            self.inventory.remove(item)
-            return item
-        return None
+        if item not in self.inventory:
+            return None
+        self.inventory.remove(item)
+        return item
 
     def get_by_id(self, id):
         for item in self.inventory:
             if item.id == id:
                 return item
         return None
-        
+
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+
+        self.remove(my_item)
+        self.add(their_item)
+
+        other_vendor.remove(their_item)
+        other_vendor.add(my_item)
+        return True
