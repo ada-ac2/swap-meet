d631936184518397e6d62ba48bc570666d008525
Soumya Sah <sah.soumya@gmail.com>
2022-12-05 22:52:46 -0800

    Add stringify method to item.py

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -3,6 +3,9 @@ class Item:
     def __init__(self, id=None):
         unique_identifier = uuid.uuid4()
         self.id = id if id else unique_identifier.int
+
     def get_category(self):
         return self.__class__.__name__
-    
+        
+    def __str__(self):
+        return f"An object of type {self.get_category()} with id {self.id}"
