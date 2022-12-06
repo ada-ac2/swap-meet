4f9feb00de876aa49e1b62eee36880ed63ca26fe
Soumya Sah <sah.soumya@gmail.com>
2022-12-06 00:42:17 -0800

    Implemented clothing.py, decor.py, electronics.py with __init__(), get_category() and __str__() methods. They all are child classes of item.py. Implemented condition_description in item.py. All tests in test_wave_05.py are passing.

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,12 @@
-class Decor:
-    pass
+from swap_meet.item import Item
+class Decor(Item):
+    def __init__(self, id=None, width=0, length=0, condition=0):
+        super().__init__(id, condition)
+        self.width = width if width else 0
+        self.length = length if length else 0
+
+    def get_category(self):
+        return f"Decor"
+
+    def __str__(self):
+        return f"An object of type {self.get_category()} with id {self.id}. It takes up a {self.width} by {self.length} sized space."
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,11 +1,30 @@
 import uuid
+import math
 class Item:
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition=0):
         unique_identifier = uuid.uuid4()
         self.id = id if id else unique_identifier.int
+        self.condition = condition if condition else 0
 
     def get_category(self):
         return self.__class__.__name__
         
     def __str__(self):
         return f"An object of type {self.get_category()} with id {self.id}"
+
+    def condition_description(self):
+        condition_rating = round(self.condition)
+        if condition_rating == 0:
+            return "mint condition"
+        elif condition_rating == 1:
+            return "as good as new"
+        elif condition_rating == 2:
+            return "used condition"
+        elif condition_rating == 3:
+            return "use at your own discretion"
+        elif condition_rating == 4:
+            return "may not be working"
+        elif condition_rating == 5:
+            return "user risk involved"
+        else:
+            return "Unknown condition"
