a8795d7d926a3b4fd03badb7f356a6c1b7949fca
larissa <larza.ault@gmail.com>
2022-12-06 19:05:48 -0800

    Finished formatting for adherence to pep8 python styling guidelines

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,17 +1,25 @@
 from swap_meet import conditions
 import uuid
 
+
 class Item:
     def __init__(self, id=None, condition=0):
-        self.id = id if id else uuid.uuid4().int
+        self.id = self.check_valid_id(id) if id else uuid.uuid4().int
         self.condition = condition
-    
+
     def __str__(self):
-        # override Item stringify method and instead return this string when str(self) called        
         return f'An object of type Item with id {self.id}'
-    
+
     def get_category(self):
         return type(self).__name__
 
     def condition_description(self):
         return conditions[self.condition]
+
+    def check_valid_id(self, id):
+        if type(id) == int:
+            return id
+        elif str(id).isnumeric():
+            return int(id)
+        else:
+            raise ValueError('id needs to be an integer value')
