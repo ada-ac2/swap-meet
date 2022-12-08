c794ccf685138491ba5a644d6b07be2c424aa242
Cindy Truong <cindythitruong@gmail.com>
2022-12-07 18:49:50 -0800

    updated __init__ constructors and key parameters

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,12 +1,12 @@
 from swap_meet.item import Item
 
 class Clothing(Item):
-    def __init__(self, id=None, condition=0.0, fabric="Unknown"):
-        super().__init__(id, condition)
-        self.fabric = fabric
+    def __init__(self, id=None, condition=None, fabric=None):
+        super().__init__(id, "Clothing", condition)
+        self.fabric = fabric if fabric is not None else "Unknown"
 
     def __str__(self):
         return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
 
     def get_category(self):
-        return "Clothing"
+        return self.category
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -2,13 +2,12 @@ from swap_meet.item import Item
 
 class Electronics(Item):
     def __init__(self, id=None, condition=0.0, type="Unknown"):
-        super().__init__(id, condition)
+        super().__init__(id, "Electronics", condition)
         self.type = type
     
     def __str__(self):
         return f"An object of type Electronics with id {self.id}. This is a {self.type} device."
 
     def get_category(self):
-        return "Electronics"
+        return self.category
 
-    
