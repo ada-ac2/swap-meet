af03dc321b07318af8d63c45151bea5ac90d223c
Elaine <sosohn@gmail.com>
2022-12-08 14:25:06 -0800

    complete unit_tests/test_wave_06

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -9,11 +9,10 @@ class Vendor:
     def remove(self, item):
         if item not in self.inventory:
             return None
-        else:
-            self.inventory.remove(item)
-            return item
+        self.inventory.remove(item)
+        return item
 
-    def get_by_id(self, id):
+    def get_by_id(self, id): #if id is None? ==> wave02
         for item in self.inventory:
             if item.id == id:            
                 return item
@@ -36,3 +35,34 @@ class Vendor:
         first_their_item = other_vendor.remove(other_vendor.inventory[0])
         self.add(first_their_item)
         return True
+
+    def get_by_category(self, category):
+        items = []
+        for item in self.inventory:
+            if item.get_category() == category:
+                items.append(item)
+        return items
+
+    def get_best_by_category(self, category):
+        items = self.get_by_category(category)
+        best_condition = 0.0  
+        best_item = None
+        for item in items:
+            if item.condition > best_condition:
+                best_item = item
+                best_condition = item.condition
+        return best_item
+
+    def swap_best_by_category(self, other_vendor, my_priority, their_priority):
+        their_priority_item = self.get_best_by_category(their_priority)
+        my_priority_item = other_vendor.get_best_by_category(my_priority)
+
+        if not other_vendor.get_by_category(my_priority) or not self.get_by_category(their_priority):
+            return False
+        
+        self.remove(their_priority_item)
+        other_vendor.remove(my_priority_item)   
+        self.add(my_priority_item)         
+        other_vendor.add(their_priority_item)
+        return True
+
