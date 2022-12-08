01701179bdc633cedfd54c96b3711fa29546142e
Sheung Wan Wong <melodywonghxy@hotmail.com>
2022-12-08 10:45:50 -0700

    wave_05 Decor and tests completed

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,13 @@
+import uuid
 class Decor:
-    pass
+    def __init__(self, id=None, width=0, length=0):
+        if id is None:
+            self.id =uuid.uuid4().int
+        else:
+            self.id = id
+        self.width = width
+        self.length = length
+    def get_category(self):
+        return "Decor"
+    def __str__(self):
+        return f"An object of type Decor with id {self.id}. It takes up a {self.width} by {self.length} sized space."
