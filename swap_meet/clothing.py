59f9b00b6178d66d4bebb87d4b28469295f26eec
Sheung Wan Wong <melodywonghxy@hotmail.com>
2022-12-08 10:31:16 -0700

    wave_05 clothing and tests completed

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,14 @@
+import uuid
 class Clothing:
-    pass
+    def __init__(self, id=None, fabric="Unknown"):
+        if id is None:
+            self.id =uuid.uuid4().int
+        else:
+            self.id = id
+        self.fabric = fabric
+    
+    def get_category(self):
+        return "Clothing"
+
+    def __str__(self):
+        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
