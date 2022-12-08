e00f34844eb019ab3443133660188b7ddf6c6220
Cindy Truong <cindythitruong@gmail.com>
2022-12-07 18:52:00 -0800

    updated constructor method and key parameters

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,15 +1,13 @@
 import uuid
 
 class Item:
-    def __init__(self, id=None, condition=0):
+    def __init__(self, id=None, category=None, condition=None):
         self.id = id if id is not None else int(uuid.uuid4())
-        self.condition = condition
+        self.condition = condition if condition is not None else 0.0
+        self.category = category if category is not None else ''
 
     def __str__(self):
         return f'An object of type Item with id {self.id}'
-
-    def get_category(self):
-        return "Item"
 
     def condition_description(self):
         if self.condition <= 1.0:
