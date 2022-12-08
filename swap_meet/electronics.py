a6d999fb3720ad56d9673b30fb71c8f391d38fcf
Sheung Wan Wong <melodywonghxy@hotmail.com>
2022-12-08 10:52:48 -0700

    wave_05 electronics and tests completed

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,12 @@
+import uuid
 class Electronics:
-    pass
+    def __init__(self, id=None, type="Unknown"):
+        if id is None:
+            self.id =uuid.uuid4().int
+        else:
+            self.id = id
+        self.type = type
+    def get_category(self):
+        return "Electronics"
+    def __str__(self):
+        return f"An object of type Electronics with id {self.id}. This is a {self.type} device."
