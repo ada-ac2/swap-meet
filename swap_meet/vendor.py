f99d867f8ea13d55adcc4a5a5e8012e736b3de82
Cindy Truong <cindythitruong@gmail.com>
2022-12-07 18:47:07 -0800

    wave 06 get_by_category, get_best_by_category, and swap_best_but_category methods in vendor class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,6 +1,5 @@
 class Vendor:
     def __init__(self, inventory=None):
-        #checks input parameters annd setting default
         self.inventory = inventory if inventory is not None else []
 
     def add(self, item):
@@ -19,13 +18,13 @@ class Vendor:
             if item.id == id:
                 return item
         return None
-    #            fatimah.swap_items(jolie, item_b, item_d)
+
     def swap_items(self, other_vendor, my_item, thier_item):
         #input: other_vendor: instance of another Vendor
         #       my_item: instance of an Item, what item 'this' vendor wants to swap
         #       thier_item: instance of an Item, what item other_vendor wants to swap
         #output: return True or False
-        if (my_item not in self.inventory) or (thier_item not in other_vendor.inventory):
+        if my_item not in self.inventory or thier_item not in other_vendor.inventory:
             return False
         else:
             self.inventory.remove(my_item)
@@ -48,3 +47,38 @@ class Vendor:
             self.inventory.append(other_vendor_first_item)
 
             return True
+    
+    #returns a list of objects in the inventory with that category
+    #                         a string
+    def get_by_category(self, category):
+        result = []
+        for item in self.inventory:
+            if item.category == category:
+                result.append(item)
+        return result
+
+    #get the item with the best condition in a certain category
+    def get_best_by_category(self, category):
+        highest_quality = 0
+        high_quality_item = None
+        for item in self.inventory:
+            if item.category == category:
+                if item.condition > highest_quality:
+                    highest_quality = item.condition
+                    high_quality_item = item
+        return high_quality_item
+
+    #other_vendor: Vendor instance we are trading with
+    #my_priority: the vendor's category we want to receive
+    #their_priority: other_vendor's category they want to receive
+    def swap_best_by_category(self, other_vendor, my_priority, their_priority):
+        if not self.inventory or not other_vendor.inventory:
+            return False
+
+        my_best = self.get_best_by_category(their_priority)
+        thier_best = other_vendor.get_best_by_category(my_priority)
+
+        res = self.swap_items(other_vendor, my_best, thier_best)
+        return res
+
+
